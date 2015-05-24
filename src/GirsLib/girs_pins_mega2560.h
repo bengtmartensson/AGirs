a1a029539518c712a7a18577061e382e20daacf8
Bengt Martensson <barf@bengt-martensson.de>
2015-05-24 18:56:14 +0200

    Renamed GirsLite in LirsLib. New files for pins.

---
+++ src/GirsLib/girs_pins_mega2560.h
@@ -0,0 +1,36 @@
+// Configuration for Arduino Nano with components soldered on the PCB.
+
+#ifndef GIRS_PINS_MEGA2560_H
+#define GIRS_PINS_MEGA2560_H
+
+#ifndef ARDUINO_AVR_MEGA2560
+#error This file is for AVR_MEGA2560 only
+#endif
+#error pattar
+#ifdef LED
+#define SIGNAL_LED_1 13
+#define SIGNAL_LED_2 A1
+#define SIGNAL_LED_3 A2
+#define SIGNAL_LED_4 A3
+#define SIGNAL_LED_5 A4
+#define SIGNAL_LED_6 A5
+#define SIGNAL_LED_7 6
+#define SIGNAL_LED_8 7
+#endif
+
+#if defined(CAPTURE)
+// capture pin (ICP) 49
+#endif
+
+#ifdef RECEIVE
+#define IRRECEIVER_PIN 5
+#endif
+#ifdef RECEIVER_2
+#define IRRECEIVER_2_PIN 12
+#endif
+
+#ifdef TRANSMIT
+// Send pin 9
+#endif
+
+#endif // ! GIRS_PINS_MEGA2560_H
---
+++ src/GirsLib/girs_pins_nano.h
@@ -0,0 +1,41 @@
+// Configuration for Arduino Nano with components soldered on the PCB.
+
+#ifndef GIRS_PINS_NANO_H
+#define GIRS_PINS_NANO_H
+
+#ifndef ARDUINO_AVR_NANO
+#error This file is for Nano only
+#endif
+
+#ifdef LED
+#define SIGNAL_LED_1 13
+#define SIGNAL_LED_2 A1
+#define SIGNAL_LED_2_GND A0
+#define SIGNAL_LED_3 A3
+#define SIGNAL_LED_3_GND A2
+#define SIGNAL_LED_4 A5
+#define SIGNAL_LED_4_GND A4
+#endif
+
+#if defined(CAPTURE)
+// capture pin (ICP) 8
+#ifndef ETHERNET
+#define SENSOR_GND 9
+#define SENSOR_VSS 10
+#endif
+#endif
+
+#ifdef RECEIVE
+#define IRRECEIVER_PIN 5
+#define IRRECEIVER_GND 6
+#define IRRECEIVER_VSS 7
+#endif
+#ifdef RECEIVER_2
+#define IRRECEIVER_2_PIN 12
+#endif
+
+#ifdef TRANSMIT
+// Send pin 3
+#endif
+
+#endif // ! GIRS_PINS_NANO_H

344aad4debe1ccb3a054c81b4df8dd9de8b40de7
Bengt Martensson <barf@bengt-martensson.de>
2015-10-26 20:27:27 +0100

    Reorganized files.

--- src/GirsLib/IrReceiver.cpp
+++
@@ -1,7 +0,0 @@
-#include "IrReceiver.h"
-
-IrReceiver::IrReceiver(uint16_t bufSize, pin_t pin_, boolean pullup, microseconds_t me) : IrReader(bufSize) {
-    pin = pin_;
-    markExcess = me;
-    pinMode(pin, pullup ? INPUT_PULLUP : INPUT);
-}

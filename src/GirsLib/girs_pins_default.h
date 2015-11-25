8b1f82e97bba2738b7ec54bd6d77b69569f233c2
Bengt Martensson <barf@bengt-martensson.de>
2015-11-25 16:44:49 +0100

    girs_pins_*.h: added busyducks.com's ascii art to these files.

--- src/GirsLib/girs_pins_default.h
+++ src/GirsLib/girs_pins_default.h
@@ -47,3 +47,33 @@
 #endif
 
 #endif // ! GIRS_PINS_DEFAULT_H
+
+/* From http://busyducks.com/ascii-art-arduinos                             +-----+
+
+         +----[PWR]-------------------| USB |--+
+         |                            +-----+  |
+         |         GND/RST2  [ ][ ]            |
+         |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5
+         |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4
+         |                             AREF[ ] |
+         |                              GND[ ] |
+         | [ ]N/C                    SCK/13[ ] |   B5
+         | [ ]v.ref                 MISO/12[ ] |   .
+         | [ ]RST                   MOSI/11[ ]~|   .
+         | [ ]3V3    +---+               10[ ]~|   .
+         | [ ]5v     | A |                9[ ]~|   .
+         | [ ]GND   -| R |-               8[ ] |   B0
+         | [ ]GND   -| D |-                    |
+         | [ ]Vin   -| U |-               7[ ] |   D7
+         |          -| I |-               6[ ]~|   .
+         | [ ]A0    -| N |-               5[ ]~|   .
+         | [ ]A1    -| O |-               4[ ] |   .
+         | [ ]A2     +---+           INT1/3[ ]~|   .
+         | [ ]A3                     INT0/2[ ] |   .
+         | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
+         | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |   D0
+         |            [ ] [ ] [ ]              |
+         |  UNO_R3    GND MOSI 5V  ____________/
+          \_______________________/
+
+*/

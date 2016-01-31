c26d95fc5935610dfe6b1f000f890f7149350242
Bengt Martensson <barf@bengt-martensson.de>
2016-01-31 12:19:50 +0100

    Removed Girs4Lirc. Note: it is replaced by GirsLite (+ AGirs).

--- src/Girs4Lirc/Girs4Lirc.cpp
+++
@@ -1,1 +0,0 @@
-../Girs/Girs.cpp
--- src/Girs4Lirc/Girs4Lirc.ino
+++
@@ -1,6 +0,0 @@
-// Dummy file to placify the silly Arduino IDE.
-// The real program resides in Girs.cpp.
-// Rationale: see http://www.gammon.com.au/forum/?id=12625
-
-#include "config.h"
-

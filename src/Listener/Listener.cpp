82bb3c7c64ad8613d52173cbdbe5265c9ec14444
Bengt Martensson <barf@bengt-martensson.de>
2016-08-22 12:06:29 +0200

    Checking in semi-working TCP Listener, before nuking the TCP code.

--- src/Listener/Listener.cpp
+++ src/Listener/Listener.cpp
@@ -16,9 +16,8 @@ this program. If not, see http://www.gnu.org/licenses/.
 */
 
 #include "config.h"
-#include <defineMissingStuff.h>
+#include "LedLcdManager.h"
 #include <GirsUtils.h>
-#include <IrReceiverSampler.h>
 
 #ifdef ARDUINO
 
@@ -47,20 +46,53 @@ this program. If not, see http://www.gnu.org/licenses/.
 #endif // ! ARDUINO
 
 #ifdef ETHERNET
-#error ETHERNET not yet supported
+#include <Ethernet.h>
+#include <IPAddress.h>
+#endif // ETHERNET
+
+#ifdef LCD
+#include <LiquidCrystal_I2C.h>
 #endif
 
-static const long serialBaud = 115200L;
-static const long serialTimeout = 5000L;
-static const milliseconds_t beginTimeout = 10000U;
-static const milliseconds_t endingTimeout = 35U;
-static const int16_t captureSize = 200U;
-static const microseconds_t markExcess = 50U;
+#ifdef RECEIVE
+#include <IrReceiverSampler.h>
+#endif
 
-IrReceiver *irReceiver = NULL;
+static const long serialBaud = SERIALBAUD;
+static const long serialTimeout = SERIALTIMEOUT;
+static const milliseconds_t beginTimeout = DEFAULT_BEGINTIMEOUT; // not really relevant here
+static const milliseconds_t endingTimeout = DEFAULT_RECEIVE_ENDINGTIMEOUT;
+static const size_t captureSize = DEFAULT_CAPTURESIZE;
+static const microseconds_t markExcess = IRRECEIVER_MARK_EXCESS;
+#ifdef BEACON
+#include <Beacon.h>
+#endif
+
+#ifdef RECEIVELED
+static LED_PARAMETER_CONST led_t receiveled = RECEIVELED;
+#endif
+
+#ifdef COMMANDLED
+static LED_PARAMETER_CONST led_t commandled = COMMANDLED;
+#endif
+
+static IrReceiver *irReceiver = NULL;
 
 #define PROGNAME "Listener"
-#define VERSION "2015-12-01"
+#define VERSION "2016-08-21"
+
+#ifdef ETHERNET
+#ifdef USEUDP
+EthernetUDP udp;
+IPAddress broadcastIp(BROADCAST_IP);
+#else // !USEUDP
+#ifdef SERVER
+static EthernetServer server(PORT);
+#else
+IPAddress peer(PEER_IP);
+#endif
+#endif // !USEUDP
+#endif // ETHERNET
 
 #ifdef ARDUINO
 void flushIn(Stream &stream) {
@@ -69,17 +101,22 @@ void flushIn(Stream &stream) {
 }
 Stream& stream = Serial;
 #else
-Stream stream(std::cout);
+static Stream stream(std::cout);
 #endif
 
-// Read and process one signal (or timeout).
-void loop() {
+static const char* readOneDecode() {
+#ifdef RECEIVELED
+    LedLcdManager::setLogicLed(receiveled, LedLcdManager::on);
+#endif
     irReceiver->enable();
     // Wait until something arrives
     while (!irReceiver->isReady())
         LedLcdManager::checkTurnoff();
 
     MultiDecoder multiDecoder(*irReceiver);
+#ifdef RECEIVELED
+    LedLcdManager::setLogicLed(receiveled, LedLcdManager::off);
+#endif
 #ifdef LCD
     if (multiDecoder.getType() > MultiDecoder::noise) {
         LedLcdManager::lcdPrint(multiDecoder.getType() == MultiDecoder::nec_ditto
@@ -90,30 +127,256 @@ void loop() {
     }
 #endif
     LedLcdManager::setLogicLed(GirsUtils::decode2logicalLed(multiDecoder.getType()), LedLcdManager::blink);
-    stream.println(multiDecoder.getDecode());
     irReceiver->disable();
+    return multiDecoder.getDecode();
 }
 
+// Read and process one signal (or timeout).
+//static void printOneDecode(Stream& stream) {
+//    const char *decode = readOneDecode();
+//    stream.println(decode);
+//}
+
 void setup() {
+    LedLcdManager::setupLedGroundPins();
     GirsUtils::setupReceivers();
     GirsUtils::setupLeds();
     LedLcdManager::setup(LCD_I2C_ADDRESS, LCD_WIDTH, LCD_HEIGHT,
             (const pin_t[]) {SIGNAL_LED_1, SIGNAL_LED_2, SIGNAL_LED_3, SIGNAL_LED_4,
                     SIGNAL_LED_5, SIGNAL_LED_6, SIGNAL_LED_7, SIGNAL_LED_8 });
     LedLcdManager::selfTest(F(PROGNAME "\n" VERSION));
-
-#ifdef ARDUINO
-    Serial.begin(serialBaud);
-    Serial.setTimeout(serialTimeout);
+#ifdef LED
+    LedLcdManager::setupShouldTimeout(receiveled, false);
+    LedLcdManager::setupShouldTimeout(commandled, false);
 #endif
+#ifdef LCD
+#ifdef ETHERNET
+#ifdef USEUDP
+    LedLcdManager::lcdPrint(F("UDP"), false, 0, 2);
+#else
+    LedLcdManager::lcdPrint(F("TCP"), false, 0, 2);
+
+#ifdef SERVER
+    LedLcdManager::lcdPrint(F(",Srv"), false);
+#else
+    LedLcdManager::lcdPrint(" " + String(peer[0], DEC) + "." + String(peer[1], DEC) + "."
+            + String(peer[2], DEC) + "." + String(peer[3], DEC) + "@" + String(PEER_PORT), false);
+#endif
+#endif
+#ifdef SERIAL_DEBUG
+    LedLcdManager::lcdPrint(F(",SerialDbg"), false);
+#endif
+#else // ! ETHERNET
+    LedLcdManager::lcdPrint(F("Serial"), false, 0, 2);
+#endif // ! ETHERNET
+#endif // LCD
+
+#ifdef ETHERNET
+#ifdef SDCARD_ON_ETHERSHIELD_PIN
+    // disable the SD card, as recommended in the doc
+    pinMode(SDCARD_ON_ETHERSHIELD_PIN, OUTPUT);
+    digitalWrite(SDCARD_ON_ETHERSHIELD_PIN, LOW);
+#endif
+    byte mac[] = { MACADDRESS };
+#ifdef DHCP
+    Ethernet.begin(mac);
+#else // !DHCP
+    Ethernet.begin(mac, IPAddress(IPADDRESS), IPAddress(DNSSERVER), IPAddress(GATEWAY), IPAddress(SUBNETMASK));
+#endif // !DHCP
+
+    String ipstring = GirsUtils::ip2string(Ethernet.localIP());
+    LedLcdManager::lcdPrint(ipstring, false, 0, 3);
+
+#ifdef BEACON
+    Beacon::setup(PROGNAME, "DE-AD-BE-EF-FE-ED", "Utility", "www.harctoolbox.org",
+            "", "", "", "http://arduino/nosuchfile.html");
+#endif
+
+#ifdef USEUDP
+#ifdef SERVER
+#error Server mode for UDP not implementd
+#endif
+#define DUMMYPORT 8888
+    udp.begin(DUMMYPORT);
+#else
+#ifdef SERVER
+    server.begin();
+#endif // SERVER
+#endif // USEUDP
+
+#endif // ETHERNET
+#if defined(ARDUINO) & !defined(ETHERNET) | defined(SERIAL_DEBUG)
+
+    Serial.begin(serialBaud);
 #if defined(ARDUINO_AVR_LEONARDO) | defined(ARDUINO_AVR_MICRO)
     while (!Serial)
         ; // wait for serial port to connect. "Needed for Leonardo only"
 #endif
+    Serial.println(F(PROGNAME " " VERSION));
+    Serial.setTimeout(serialTimeout);
+
+#ifdef ETHERNET
+    Serial.println(Ethernet.localIP());
+#endif
+#endif // defined(ARDUINO) & !defined(ETHERNET) | defined(SERIAL_DEBUG)
+
     stream.println(F(PROGNAME " " VERSION));
 
     irReceiver = IrReceiverSampler::newIrReceiverSampler(captureSize, IRRECEIVER_1_PIN,
             IRRECEIVER_1_PULLUP_VALUE, markExcess, beginTimeout, endingTimeout);
+}
+
+void readPrintOneCommand(Stream& stream) {
+    //    String line = readCommand(stream);
+    //#ifdef SERIAL_DEBUG
+    //    Serial.println("Command: " + line);
+    //#endif
+    const char* decode = readOneDecode();
+    stream.println(decode);
+    //return true;
+}
+
+#if defined(ETHERNET)
+#ifdef USEUDP
+void readPrintOneUdpCommand() {
+    const char *decode = readOneDecode();
+    udp.beginPacket(broadcastIp, BROADCAST_PORT);
+    udp.write(decode);
+    udp.write(EOLCHAR);
+    udp.endPacket();
+}
+#else // ! USEUDP
+boolean readProcessOneTcpCommand(EthernetClient& client) {
+    while (client.available() > 0)
+        client.read();
+
+    //    LedLcdManager::checkTurnoff();
+#ifdef BEACON
+        Beacon::checkSend();
+#endif
+        if (!client.connected())
+            return false;
+    //}
+    readPrintOneCommand(client);
+    return true;
+}
+#endif // ! USEUDP
+#endif // ETHERNET
+
+void loop() {
+    LedLcdManager::checkTurnoff();
+#ifdef ETHERNET
+#ifdef BEACON
+    Beacon::checkSend();
+#endif
+#ifdef USEUDP
+#ifdef SERVER
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+        IPAddress remote = udp.remoteIP();
+#ifdef SERIAL_DEBUG
+        Serial.print("Received packet of size ");
+        Serial.println(packetSize);
+        Serial.print("From ");
+        Serial.print(remote);
+        Serial.print(", port ");
+        Serial.println(udp.remotePort());
+#endif
+        LedLcdManager::lcdPrint("UDP: " + ip2string(remote), true, 0, 0); // TODO: #ifdef...
+        LedLcdManager::lcdPrint("@" + String(udp.remotePort(), DEC), false, 0, 1);
+        String peek(char(udp.peek()));
+        LedLcdManager::lcdPrint(peek, false, 0, 2);
+
+        udp.beginPacket(udp.remoteIP(), udp.remotePort());
+        readProcessOneCommand(udp);
+        udp.endPacket();
+    } else {
+        delay(10);
+        LedLcdManager::checkTurnoff();
+    }
+#endif // SERVER
+    readPrintOneUdpCommand();
+#else // ! USEUDP
+
+#ifdef SERVER
+    EthernetClient client = server.available();
+    if (!client)
+        return;
+    client.setTimeout(10000);
+#ifdef LCD
+    LedLcdManager::lcdPrint(F("Connection!"), true, 0, 0);
+#endif
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Connection!"));
+#endif
+    client.println(F(PROGNAME));
+#if defined(COMMANDLED) & defined(LED)
+    LedLcdManager::setLogicLed(commandled, LedLcdManager::on);
+#endif
+
+    //while (client.read() != -1)
+    //    LedLcdManager::checkTurnoff();
+#ifdef SESSION
+    while (readProcessOneTcpCommand(client))
+#if defined(COMMANDLED) & defined(LED)
+        LedLcdManager::setLogicLed(commandled, LedLcdManager::on)
+#endif
+        ;
+#else
+    readProcessOneTcpCommand(client);
+#endif
+#ifdef LCD
+    LedLcdManager::lcdPrint(F("Connection closed!"), true, 0, 0);
+#endif
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Connection closed!"));
+#endif
+    //client.println(F("Bye"));
+    client.stop();
+
+#if defined(COMMANDLED) & defined(LED)
+    LedLcdManager::setLogicLed(commandled, LedLcdManager::off);
+#endif
+
+#else // !SERVER
+    IPAddress peer(PEER_IP);
+    EthernetClient client;
+    boolean status = client.connect(peer, PEER_PORT);
+    if (!status)
+        return;
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Connection!"));
+#endif
+#ifdef LCD
+    LedLcdManager::lcdPrint(F("Connection!"), true, 0, 0);
+#endif
+
+#ifdef SESSION
+    while
+#endif
+        (readProcessOneTcpCommand(client))
+        ;
+#endif // !SERVER
+    if (client.connected())
+        client.flush();
+    client.stop();
+#ifdef LCD
+    LedLcdManager::lcdPrint(F("Connection closed!"), true, 0, 0);
+#endif
+#ifdef SERIAL_DEBUG
+    Serial.println(F("Connection closed!"));
+#endif
+
+#endif // !USEUDP
+#else // ! ETHERNET
+
+#ifdef ARDUINO
+    Stream& stream = Serial;
+ #else
+    Stream stream(std::cout);
+#endif
+   readPrintOneCommand(stream);
+#endif // ! ETHERNET
 }
 
 #ifndef ARDUINO
--- src/Listener/config.h
+++ src/Listener/config.h
@@ -3,49 +3,90 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// Properties, needed for the *_pins.h files
+// Define for pure listener
+#define LISTENER
+
+// Displayed in greeting messages
+//#define PROGNAME "Listener"
+
+// Receive demodulated IR signals, require demodulating IR sensor
 #define RECEIVE
-#define DECODER
+
+// Use LEDs
 #define LED
+
+#ifdef LED
+#define DECODELED
+
+// Light this when receive is pending
+#define RECEIVELED 1
+#define COMMANDLED 5
+#endif // LED
+
+// LCD display with I2C connection.
 #define LCD
 
-// Using Ethernet for communication
-//#define ETHERNET
-
-// Parameters, (not really defaults, but actual values))
-#ifndef ETHERNET
-#define SERIALBAUD 115200
-#define SERIALTIMEOUT 5000L
-#endif // !defined ETHERNET
-
-#define DEFAULT_RECEIVE_ENDINGTIMEOUT 35L // milliseconds
-#define DEFAULT_CAPTURESIZE 200U // must be even
-
-// This quantity is added to all gaps and subtracted from all marks.
-#define IRRECEIVER_MARK_EXCESS 50U
-
+// Define if using Ethernet (TCP or UDP) as the communication channel
+#define ETHERNET
 
 #ifdef ETHERNET
 
 // Issues some debug messages on the serial port
-//#define SERIAL_DEBUG
+#define SERIAL_DEBUG
 
 // If SERVER but not USEUDP are defined, the progam runs in "telnet" mode,
 // waiting for incoming tcp connections on port PORT.  If none is
 // defined, try to initiate tcp connection to PEER_IP, port
 // PEER_PORT. In both cases, connection is interrupted when input is
 // received.  If USEUDP is defined, use UDP instead. Input is then ignored.
+// For some reason I do not understand, when SERVER is defined, the first line
+// is discarded by server.available().
 
-#define SERVER // Wait for incoming connections, for "telnet-ting" to the Arduino
-               // If not the Arduino tries to establish a connection to PEER_IP at port PEER_PORT
+// multi-command session, otherwise close after one command. Ignored if USEUDP defined,
+// while meaningless.
+#define SESSION
+
+// Wait for incoming connections, for "telnet-ting" to the Arduino
+// If not defined, the Arduino tries to establish a connection to PEER_IP at port PEER_PORT
+//#define SERVER
 // NOTE: do NOT use UDP as preprocessor symbol, it makes Ethernet.h go haywire.
 //#define USEUDP
-//#define ETHER_W5100 // Use normal Arduino Ethernet library, usin W5100 and pin 10
-//#define SDCARD_ON_ETHERSHIELD // there is a pin4 to be selected low
-//#define ETHER_ENC28J60 // Cheapie ENC 28J60 chip, not yet supported
-#define ETHERNET_SESSION // multi-command session, otherwise close after one command
-//#define DHCP // Consumes rather much memory
+#define SDCARD_ON_ETHERSHIELD // there is a pin4 to be selected low
 
+#define SDCARD_ON_ETHERSHIELD_PIN 4 // there is a pin4 to be selected low
+#define DHCP // Consumes rather much memory
+
+// Invoke the AMX style beacon, as per https://github.com/bengtmartensson/ABeacon
+//#define BEACON
+
+#endif // ETHERNET
+
+// Hardware configuration
+// Include one file describing the pin configuration
+// Use one in GirsLib, or write your own.
+#ifdef ARDUINO
+
+#include <girs_pins.h> // Generic
+
+#else // ! ARDUINO
+// For compiling a version to test on the PC, not to flash onto the Arduino.
+#include <girs_pins_dummy.h>
+#endif // ! ARDUINO
+
+// If LCD support desired, include appropriate hardware description
+#ifdef LCD
+//#include <lcd_0x27_16_2.h>
+#include <lcd_0x27_20_4.h>
+#endif
+
+// Character that ends the command lines
+#define EOLCHAR '\n'
+
+#ifdef ETHERNET
+#ifdef USEUDP
+#define BROADCAST_IP 192,168,1,3
+#define BROADCAST_PORT 55555
+#else // !USEUDP
 #ifndef SERVER
 // Host the program tries to contact
 #define PEER_IP 192,168,1,3
@@ -53,6 +94,7 @@
 // Port to be listened on
 #define PEER_PORT 44444
 #endif
+#endif // !USEUDP
 
 #define MACADDRESS 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 #ifndef DHCP
@@ -61,27 +103,19 @@
 #define DNSSERVER  192,168,1,4
 #define SUBNETMASK 255,255,255,0
 #endif // ! DHCP
-#define PORT       33333
+#define PORT       44444
 #endif // ETHERNET
 
-// Hardware configuration
-// Include one file describing the pin configuration
-#ifdef ARDUINO
-//#include <girs_pins_lcdshield.h>
-#include <girs_pins_nano_shield.h>
-//#include <girs_pins_nano_alt.h>
-//#include <girs_pins.h> // Generic
-#else // !  ARDUINO
-#include <girs_pins_dummy.h>
-#endif
+#define DEFAULT_RECEIVE_ENDINGTIMEOUT 35L // milliseconds
+#define DEFAULT_CAPTURESIZE 200U // must be even
+#define DEFAULT_BEGINTIMEOUT 10000UL // milliseconds
+// This quantity is added to all gaps and subtracted from all marks.
+#define IRRECEIVER_MARK_EXCESS 50
 
-// If LCD support desired, include appropriate hardware description
-#ifdef LCD
-#ifdef ARDUINO_AVR_NANO
-#include <lcd_0x27_16_2.h>
-#else
-#include <lcd_0x3F_20_4.h>
-#endif
-#endif
+#if !defined(ETHERNET) | defined(SERIAL_DEBUG)
+// Parameters, (not really defaults, but actual values))
+#define SERIALBAUD 115200UL
+#define SERIALTIMEOUT 5000ULL
+#endif // !defined(ETHERNET) | defined(SERIAL_DEBUG)
 
 #endif // ! CONFIG_H

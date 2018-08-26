546657acf3a0b2528ad9f6e60235b4310a9bc21b
Bengt Martensson <barf@bengt-martensson.de>
2018-08-26 18:01:33 +0200

    LedLcdManager: fixes for LCD not defined.

--- src/GirsLib/LedLcdManager.cpp
+++ src/GirsLib/LedLcdManager.cpp
@@ -124,7 +124,11 @@ void LedLcdManager::selfTest(const char *text) {
     lcdPrint(text);
     for (led_t i = 1; i <= maxLeds; i++)
         setLogicLed(i, on);
-    delay(lcd ? selftestTimeWithLCD : selftestTimeWithoutLCD);
+    delay(
+#ifdef LCD
+            lcd ? selftestTimeWithLCD :
+#endif
+                selftestTimeWithoutLCD);
     allOff(true);
 }
 
@@ -133,7 +137,11 @@ void LedLcdManager::selfTest(const __FlashStringHelper *text) {
     lcdPrint(text);
     for (led_t i = 1; i <= maxLeds; i++)
         setLogicLed(i, on);
-    delay(lcd ? selftestTimeWithLCD : selftestTimeWithoutLCD);
+    delay(
+#ifdef LCD
+    lcd ? selftestTimeWithLCD :
+#endif
+        selftestTimeWithoutLCD);
     allOff(true);
 }
 #endif
